31105ae35a7580c5950776c02df1180d22f96d07
EC2 Default User <ec2-user@ip-172-31-31-123.ap-northeast-1.compute.internal>
2023-05-07 05:49:56 +0000

    Initial Commit

---
+++ lesson5.rb
@@ -0,0 +1,13 @@
+total_price=100
+
+if total_price>100
+  puts "みかんを購入。所持金に余りあり。"
+end
+
+if total_price == 100
+  puts "みかんを購入。所持金は0円。"
+end
+
+if total_price < 100
+  puts "みかんを購入することが出来ません。"
+end
